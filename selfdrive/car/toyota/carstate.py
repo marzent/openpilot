84cbf25ea1ba4c0278345c5aa5aaa15c471aec7c
Marc-Aurel Zent <marc_aurel@me.com>
2019-06-03 23:15:50 +0200

    Distance test

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -171,4 +171,4 @@ class CarState(object):
       self.generic_toggle = cp.vl["AUTOPARK_STATUS"]['STATE'] != 0
     else:
       self.generic_toggle = bool(cp.vl["LIGHT_STALK"]['AUTO_HIGH_BEAM'])
-    self.lk_mode = bool(cp.vl["LKAS_HUD"]['LEFT_LINE'])
+    self.lk_mode = cp.vl["ACC_CONTROL"]['DISTANCE']
