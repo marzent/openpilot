fc667bb904b0e6c3d3a6b9358348f4d7b23145dd
Marc-Aurel <marc_aurel@me.com>
2019-10-21 15:18:53 +0200

    blinker cuts steering

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -390,6 +390,8 @@ class CarInterface(CarInterfaceBase):
       events.append(create_event('wrongCarMode', [ET.NO_ENTRY, ET.USER_DISABLE]))
     if ret.gearShifter == GearShifter.reverse and self.CP.enableDsu:
       events.append(create_event('reverseGear', [ET.NO_ENTRY, ET.IMMEDIATE_DISABLE]))
+    if self.CS.left_blinker_on or self.CS.right_blinker_on:
+      events.append(create_event('manualSteeringRequiredBlinkersOn', [ET.WARNING]))
     if self.CS.steer_error:
       events.append(create_event('steerTempUnavailable', [ET.NO_ENTRY, ET.WARNING]))
     if self.CS.low_speed_lockout and self.CP.enableDsu:
